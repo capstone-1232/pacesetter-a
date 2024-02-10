3e18c9b8d5e9125808da2e79810006443bae31ec
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2024-02-10 11:57:17 -0700

    index/style

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package pacesetter
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+*
+?>
