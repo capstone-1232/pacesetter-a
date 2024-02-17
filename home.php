0cab0dbd4c0b253bb47782256b3fd2bfbd6fa5d9
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2024-02-17 15:23:23 -0700

    home

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * pacesetter front page
+ *
+ *
+ * @package pacesetter
+ */
+
+/**
+ */
+get_header();
+
+?>
+<div class="banner"></div>
+
+<h1>Featured Items</h1>
+
+
+<?php get_footer(); ?>
