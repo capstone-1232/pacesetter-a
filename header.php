14792d1b3e4cdf3494880df2efa9e29b5bf7163b
Hyeonseo <tym30003@gmail.com>
2024-03-08 07:44:01 -0700

    3rd time commitment

--- header.php
+++ header.php
@@ -1,144 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<?php wp_head(); ?>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
-	<title>Pacesetter Ski & Snowboard</title>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
-</head>
-<body <?php body_class(); ?>>
-<!-- retrieves the header image. get_custom_header() gets the custom header data. -->
-<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-			<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-		</a>
-	</div>
-<?php endif; ?>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Pacesetter | Home</title>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <header>
 
-	<div class="p-menu">
-		<nav id="navbar" class="navigation">
-				
-		<!-- custom logo -->
-		<?php if ( function_exists( 'the_custom_logo' ) ) {
-			the_custom_logo();
-		} ?>
-
-	<div class="nav-items">
-		<div class="subnav">
-			<a class="subnavbtn" href="#">
-				Skis<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Skis</a>
-			<a href="#">Ski Boots</a>
-			<a href="#">Ski Bindings</a>
-			<a href="#">Poles</a>
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Snowboards<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Snowboards</a>
-			<a href="#">Snowboard Boots</a>
-			<a href="#">Snowboard Bindings</a>
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Apparel<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Mens</a>
-			<a href="#">Womens</a>
-			<a href="#">Kids</a>
-			<a href="#">Accessories</a>`
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Accessories<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Bags</a>
-			<a href="#">Helmets</a>
-			<a href="#">Goggles</a>`
-		</div> <!-- subnav-content end -->
-			<a href="#">Services</a>
-			<a href="#">Rentals</a>
-		</div> <!-- sub-nav end -->
-	</div> <!-- nav-items end -->
-		
-	<!-- hamburger toggle button -->
-	<!-- <input id="toggle" type="checkbox" />
-	<label class="hamburger" for="toggle">
-		<div class="top"></div>
-		<div class="middle"></div>
-		<div class="bottom"></div>
-	</label> -->
-</nav> <!-- end main nav -->
-	
-    <nav class="menu">
-		<a class="subnavbtn" href="#">
-			Skis<i class="fa fa-caret-down"></i>
-		</a>
-	<div class="subnav-content">
-	  	<a href="#">Skis</a>
-      	<a href="#">Ski Boots</a>
-      	<a href="#">Ski Bindings</a>
-	  	<a href="#">Poles</a>`
-    </div>
-		<a class="subnavbtn" href="#">
-			Snowboards<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Snowboards</a>
-      	<a href="#">Snowboard Boots</a>
-      	<a href="#">Snowboard Bindings</a>
-    </div>
-		<a class="subnavbtn" href="#">
-			Apparel<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Mens</a>
-      	<a href="#">Womens</a>
-      	<a href="#">Kids</a>
-	  	<a href="#">Accessories</a>`
-	</div>
-		<a class="subnavbtn" href="#">
-			Accessories<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Bags</a>
-      	<a href="#">Helmets</a>
-	  	<a href="#">Goggles</a>`
-    </div>
-	<a class="link" href="#">Events</a>
-	<a class="link" href="https://pacesetter-a.web.dmitcapstone.ca/wordpress/contact/">Contact Us</a>
-	<a class="link" href="https://pacesetter-a.web.dmitcapstone.ca/wordpress/about-us/">About Us</a>
-	<a class="link" href="https://pacesetter-a.web.dmitcapstone.ca/wordpress/rentals/">Rentals</a>
-	<a class="link" href="https://pacesetter-a.web.dmitcapstone.ca/wordpress/faq/">FAQ</a>
-	<a class="link" href="https://pacesetter-a.web.dmitcapstone.ca/wordpress/services/">Services</a>
-    </nav>
-</div>
-
-<?php
-$header_info = array(
-	'width'         => 980,
-	'height'        => 60,
-	'default-image' => get_template_directory_uri() . '/images/sunset.jpg',
-);
-add_theme_support( 'custom-header', $header_info );
-
-// custom header image might not need
-// $header_images = array(
-// 	'sunset' => array(
-// 		'url'           => get_template_directory_uri() . '/images/sunset.jpg',
-// 		'thumbnail_url' => get_template_directory_uri() . '/images/sunset_thumbnail.jpg',
-// 		'description'   => 'Sunset',
-// 	),
-// 	'flower' => array(
-// 		'url'           => get_template_directory_uri() . '/images/flower.jpg',
-// 		'thumbnail_url' => get_template_directory_uri() . '/images/flower_thumbnail.jpg',
-// 		'description'   => 'Flower',
-// 	),
-// );
-// register_default_headers( $header_images );
+        </header>
