0f31cc49d4b490d0648a7596cff234d94e5fb1a0
ConnerSaj <consaj72@gmail.com>
2024-03-13 14:53:45 -0600

    Added single page view.  Added shortcode for form on contact page

---
+++ page-single.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * pacesetter services page
+ *
+ *
+ * @package pacesetter
+ */
+
+/**
+ */
+
+ get_header();
+
+ ?>
+
+<!-- Product Image -->
+ <div>
+
+
+ </div>
+
+ <!-- Product Description -->
+ <div>
+
+
+ </div>
+
+ <!-- Product Specifications -->
+ <div>
+
+
+ </div>
+
+<?php get_footer(); ?>
