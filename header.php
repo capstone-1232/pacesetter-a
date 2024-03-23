c5a5310d182e63d0c0962efb33e4c42f865bbf3d
TheBraveFrog <tonypedro59@gmail.com>
2024-03-22 22:21:25 -0600

    nav styling start

--- header.php
+++ header.php
@@ -12,134 +12,120 @@
 	<script>js/functions.js</script>
 </head>
 <body <?php body_class(); ?>>
-<!-- retrieves the header image. get_custom_header() gets the custom header data. -->
-<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-			<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-		</a>
-	</div>
-<?php endif; ?>
-	<div class="p-menu">
-		<h1 class="nav-hidden">Top Nav</h1>
-		<nav id="navbar" class="navigation">		
-		<!-- custom logo -->
-		<?php if ( function_exists( 'the_custom_logo' ) ) {
-			the_custom_logo();
-		} ?>
-
-	<div class="nav-items">
-		<div class="subnav">
-			<a class="subnavbtn" href="#">
-				Skis<i class="fa fa-caret-down"></i>
+<header>
+	<!-- retrieves the header image. get_custom_header() gets the custom header data. -->
+	<?php if ( get_header_image() ) : ?>
+		<div id="site-header">
+				<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 			</a>
-		<div class="subnav-content">
-			<a href="#">Skis</a>
-			<a href="#">Ski Boots</a>
-			<a href="#">Ski Bindings</a>
-			<a href="#">Poles</a>
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Snowboards<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Snowboards</a>
-			<a href="#">Snowboard Boots</a>
-			<a href="#">Snowboard Bindings</a>
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Apparel<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Mens</a>
-			<a href="#">Womens</a>
-			<a href="#">Kids</a>
-			<a href="#">Accessories</a>`
-		</div> <!-- subnav-content end -->
-			<a class="subnavbtn" href="#">
-				Accessories<i class="fa fa-caret-down"></i>
-			</a>
-		<div class="subnav-content">
-			<a href="#">Bags</a>
-			<a href="#">Helmets</a>
-			<a href="#">Goggles</a>`
-		</div> <!-- subnav-content end -->
-			<a href="#">Services</a>
-			<a href="#">Rentals</a>
-		</div> <!-- sub-nav end -->
-	</div> <!-- nav-items end -->
-		
-	<!-- hamburger toggle button -->
-	<!-- <input id="toggle" type="checkbox" />
-	<label class="hamburger" for="toggle">
-		<div class="top"></div>
-		<div class="middle"></div>
-		<div class="bottom"></div>
-	</label> -->
-</nav> <!-- end main nav -->
+		</div>
+	<?php endif; ?>
+		<div class="p-menu">
+			<h1 class="nav-hidden">Top Nav</h1>
+			<nav id="navbar" class="navigation">
+			<!-- custom logo -->
+			<?php if ( function_exists( 'the_custom_logo' ) ) {
+				the_custom_logo();
+			} ?>
 	
-    <nav class="menu">
-		<a class="subnavbtn" href="#">
-			Skis<i class="fa fa-caret-down"></i>
-		</a>
-	<div class="subnav-content">
-	  	<a href="#">Skis</a>
-      	<a href="#">Ski Boots</a>
-      	<a href="#">Ski Bindings</a>
-	  	<a href="#">Poles</a>`
-    </div>
-		<a class="subnavbtn" href="#">
-			Snowboards<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Snowboards</a>
-      	<a href="#">Snowboard Boots</a>
-      	<a href="#">Snowboard Bindings</a>
-    </div>
-		<a class="subnavbtn" href="#">
-			Apparel<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Mens</a>
-      	<a href="#">Womens</a>
-      	<a href="#">Kids</a>
-	  	<a href="#">Accessories</a>`
-	</div>
-		<a class="subnavbtn" href="#">
-			Accessories<i class="fa fa-caret-down"></i>
-		</a>
-    <div class="subnav-content">
-      	<a href="#">Bags</a>
-      	<a href="#">Helmets</a>
-	  	<a href="#">Goggles</a>`
-    </div>
-	<a class="link" href="events">Events</a>
-	<a class="link" href="contact">Contact Us</a>
-	<a class="link" href="about-us">About Us</a>
-	<a class="link" href="rentals">Rentals</a>
-	<a class="link" href="faq">FAQ</a>
-	<a class="link" href="services">Services</a>
-    </nav>
-</div>
+		<div class="nav-items">
+			<div class="subnav">
+				<a class="subnavbtn" href="#">
+					Skis<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Skis</a>
+				<a href="#">Ski Boots</a>
+				<a href="#">Ski Bindings</a>
+				<a href="#">Poles</a>
+			</div> <!-- subnav-content end -->
+				<a class="subnavbtn" href="#">
+					Snowboards<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Snowboards</a>
+				<a href="#">Snowboard Boots</a>
+				<a href="#">Snowboard Bindings</a>
+			</div> <!-- subnav-content end -->
+				<a class="subnavbtn" href="#">
+					Apparel<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Mens</a>
+				<a href="#">Womens</a>
+				<a href="#">Kids</a>
+				<a href="#">Accessories</a>`
+			</div> <!-- subnav-content end -->
+				<a class="subnavbtn" href="#">
+					Accessories<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Bags</a>
+				<a href="#">Helmets</a>
+				<a href="#">Goggles</a>`
+			</div> <!-- subnav-content end -->
+				<a href="#">Services</a>
+				<a href="#">Rentals</a>
+			</div> <!-- sub-nav end -->
+		</div> <!-- nav-items end -->
+	</nav> <!-- end main nav -->
 
-<?php
-$header_info = array(
-	'width'         => 980,
-	'height'        => 60,
-	'default-image' => get_template_directory_uri() . '/images/sunset.jpg',
-);
-add_theme_support( 'custom-header', $header_info );
-
-// custom header image might not need
-// $header_images = array(
-// 	'sunset' => array(
-// 		'url'           => get_template_directory_uri() . '/images/sunset.jpg',
-// 		'thumbnail_url' => get_template_directory_uri() . '/images/sunset_thumbnail.jpg',
-// 		'description'   => 'Sunset',
-// 	),
-// 	'flower' => array(
-// 		'url'           => get_template_directory_uri() . '/images/flower.jpg',
-// 		'thumbnail_url' => get_template_directory_uri() . '/images/flower_thumbnail.jpg',
-// 		'description'   => 'Flower',
-// 	),
-// );
-// register_default_headers( $header_images );
+	<div class="flex-container">
+					<img src="img/logo.svg" alt="Brand Logo">
+			<a class="nav-btn">
+				<img src="img/hamburger-menu.svg" alt="hamburger-menu icon">
+			</a>
+			<nav class="menu">
+				<a class="subnavbtn" href="#">
+					Skis<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Skis</a>
+				<a href="#">Ski Boots</a>
+				<a href="#">Ski Bindings</a>
+				<a href="#">Poles</a>`
+			</div>
+				<a class="subnavbtn" href="#">
+					Snowboards<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Snowboards</a>
+				<a href="#">Snowboard Boots</a>
+				<a href="#">Snowboard Bindings</a>
+			</div>
+				<a class="subnavbtn" href="#">
+					Apparel<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Mens</a>
+				<a href="#">Womens</a>
+				<a href="#">Kids</a>
+				<a href="#">Accessories</a>`
+			</div>
+				<a class="subnavbtn" href="#">
+					Accessories<i class="fa fa-caret-down"></i>
+				</a>
+			<div class="subnav-content">
+				<a href="#">Bags</a>
+				<a href="#">Helmets</a>
+				<a href="#">Goggles</a>`
+			</div>
+			<a class="link" href="events">Events</a>
+			<a class="link" href="contact">Contact Us</a>
+			<a class="link" href="about-us">About Us</a>
+			<a class="link" href="rentals">Rentals</a>
+			<a class="link" href="faq">FAQ</a>
+			<a class="link" href="services">Services</a>
+			</nav>
+		</div>
+		</div>
+	
+	<?php
+		$header_info = array(
+			'width'         => 980,
+			'height'        => 60,
+			'default-image' => get_template_directory_uri() . '/images/sunset.jpg',
+		);
+		add_theme_support( 'custom-header', $header_info );
+	?>
+</header>
