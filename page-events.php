0db6f0ef4b3693905eed60ec03e9c29862f01aa6
Hyeonseo Kim <tym30003@gmail.com>
2024-03-28 15:08:15 -0600

    Update page-events.php

--- page-events.php
+++ page-events.php
@@ -26,66 +26,34 @@
     </div>
     <div class="event-card">
     <?php 
-    
-    if (isset($_GET['calendar-select'])) {
-        switch($_GET['calendar-select']) {
-            case 'calendar':
-                echo do_shortcode("[calendar id='353']");
-            break;
 
-            case 'event-card':
-
-                $args = array(
-                    'posts_per_page'    => -1,
-                    'post_type'     => 'event'
-                );
-
-                $the_query = new WP_Query( $args );
-
-                if( $the_query->have_posts() ):
-                    echo '<ul>';
-                    while( $the_query->have_posts() ) : $the_query->the_post();
-                    echo    '<li class="event-card-list">';
-                        the_post_thumbnail();
-                    echo        '<div class="card-body">';
-                        the_title();
-                        the_content();
-                    echo do_shortcode('[email-subscribers-form id="3"]');
-                    echo        '</div>';
-                    echo    '</li>';
-                    endwhile;
-                    echo '</ul>';
-                endif;
-
-                wp_reset_query();
-            break;
-        }
-
-    } else {
-        $args = array(
-                    'posts_per_page'    => -1,
-                    'post_type'     => 'event'
-                );
-
-                $the_query = new WP_Query( $args );
-
-                if( $the_query->have_posts() ):
-                    echo '<ul>';
-                    while( $the_query->have_posts() ) : $the_query->the_post();
-                    echo    '<li class="event-card-list">';
-                        the_post_thumbnail();
-                    echo        '<div class="card-body">';
-                        the_title();
-                        the_content();
-                    echo do_shortcode('[email-subscribers-form id="3"]');
-                    echo        '</div>';
-                    echo    '</li>';
-                    endwhile;
-                    echo '</ul>';
-                endif;
-
-                wp_reset_query();
+    if (isset($_GET['calendar-select']) && option=="calendar") {
+        echo do_shortcode("[calendar id='353']");
     }
+
+        $args = array(
+                        'posts_per_page'    => -1,
+                        'post_type'     => 'event'
+                    );
+
+                    $the_query = new WP_Query( $args );
+
+                    if( $the_query->have_posts() ):
+                        echo '<ul>';
+                        while( $the_query->have_posts() ) : $the_query->the_post();
+                        echo    '<li class="event-card-list">';
+                            the_post_thumbnail();
+                        echo        '<div class="card-body">';
+                            the_title();
+                            the_content();
+                        echo do_shortcode('[email-subscribers-form id="3"]');
+                        echo        '</div>';
+                        echo    '</li>';
+                        endwhile;
+                        echo '</ul>';
+                    endif;
+
+                    wp_reset_query();
 
     ?>
     </div>
