9d55013c5e4f76dab8131f65d627014f05dcc4e5
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2024-03-28 19:53:16 -0600

    removing functions

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,9 @@ var modal = document.getElementsByClassName("emaillist");
 var span = document.getElementsByClassName("sucess")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
