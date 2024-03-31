dcef2f2bc00209e1bd71b2467b87f1e1f43d3ca7
Hyeonseo <tym30003@gmail.com>
2024-03-31 11:32:56 -0600

    Up

--- front-page.php
+++ front-page.php
@@ -20,6 +20,7 @@
         <!-- featured -->
         <div class="featured">
             <h1>Featured</h1>
+            <div class="featured-products">
             <?php  
                 $args = array(
                     'post_type'      => 'product',
@@ -31,21 +32,22 @@
 
                 while ( $loop->have_posts() ) : $loop->the_post();
                     global $product;
-                    echo '<div class="featured-product">' . woocommerce_get_product_thumbnail() . '<p class="featured-title">' . get_the_title() . '</p> <p class="featured-price">' . wc_get_price_to_display( $product, array( 'price' => $product->get_price() ) ) . '</p> </div>';
+                    echo '<div class="featured-card">' . woocommerce_get_product_thumbnail() . '<p class="featured-title">' . get_the_title() . '</p> <p class="featured-price">' . wc_get_price_to_display( $product, array( 'price' => $product->get_price() ) ) . '</p> </div>';
                 endwhile;
 
                 wp_reset_query();
             ?>
+            </div>
         </div>
 
         <!-- top brands -->
         <div class="top-brands">
             <h2>Shop Top Brands</h2>
             <ul class="slider">
-                <li class="slider-item"><img src="https://drive.google.com/uc?export=view&id=1G7zeotJ6w6h5NsdfA9MpZEinbA45fCBG"></li>
-                <li class="slider-item"><img src="https://drive.google.com/uc?export=view&id=1uNRvtgX89B3KmQHGJQpLDoNPgFH1oCOu"></li>
-                <li class="slider-item"><img src="https://drive.google.com/uc?export=view&id=1Cp3Jobqybs2KGKm7DTutvmZkWi7c-50Y"></li>
-                <li class="slider-item"><img src="https://drive.google.com/uc?export=view&id=135lHXV8_QtuyrKtCH_vo7kK8D-6yw45L"></li>
+                <li class="slider-item"><img src="https://picsum.photos/200"></li>
+                <li class="slider-item"><img src="https://picsum.photos/200"></li>
+                <li class="slider-item"><img src="https://picsum.photos/200"></li>
+                <li class="slider-item"><img src="https://picsum.photos/200"></li>
             </ul>
         </div>
     </div>
