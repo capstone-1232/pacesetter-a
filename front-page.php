49f33b5543200ee5bee24fae346b8f258a9d6057
Hyeonseo <tym30003@gmail.com>
2024-03-30 20:17:55 -0600

    Featured Item Update

--- front-page.php
+++ front-page.php
@@ -20,6 +20,22 @@
         <!-- featured -->
         <div class="featured">
             <h1>Featured</h1>
+            <?php  
+                $args = array(
+                    'post_type'      => 'product',
+                    'posts_per_page' => 4,
+                    'product_cat'    => 'featured-item'
+                );
+
+                $loop = new WP_Query( $args );
+
+                while ( $loop->have_posts() ) : $loop->the_post();
+                    global $product;
+                    echo '<br /><a href="'.get_permalink().'">' . woocommerce_get_product_thumbnail().' '.get_the_title().'</a>';
+                endwhile;
+
+                wp_reset_query();
+            ?>
         </div>
 
         <!-- top brands -->
