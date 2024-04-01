9a7795cf4426ce8165deab67ba6b2602d0362699
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2024-04-01 15:13:46 -0600

    lightspeed optomizations

--- js/functions.js
+++ js/functions.js
@@ -46,3 +46,41 @@ subnav4.addEventListener('click', function() {
   menuContent4.classList.toggle('menu-expanded-small');
 });
 });
+
+// debounce scroll handlers
+function onScroll (evt) {
+
+  lastScrollY = window.scrollY;
+
+  // Prevent multiple rAF callbacks.
+  if (scheduledAnimationFrame)
+  return;
+
+  scheduledAnimationFrame = true;
+  requestAnimationFrame(readAndUpdatePage);
+}
+
+window.addEventListener('scroll', onScroll);
+
+// lazy load
+// document.addEventListener("DOMContentLoaded", function() {
+//   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+//   if ("IntersectionObserver" in window) {
+//     let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+//       entries.forEach(function(entry) {
+//         if (entry.isIntersecting) {
+//           let lazyImage = entry.target;
+//           lazyImage.src = lazyImage.dataset.src;
+//           lazyImage.srcset = lazyImage.dataset.srcset;
+//           lazyImage.classList.remove("lazy");
+//           lazyImageObserver.unobserve(lazyImage);
+//         }
+//       });
+//     });
+
+//     lazyImages.forEach(function(lazyImage) {
+//       lazyImageObserver.observe(lazyImage);
+//     });
+//   }
+// });
